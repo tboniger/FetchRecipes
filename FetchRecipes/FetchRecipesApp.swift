e5bbd3cbd57db992ac12bf40b553df6c8f5d799f
Timothy Boniger <tboniger@gmail.com>
2024-10-19 13:01:33 -0400

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Timothy Böniger on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
